5c62ea36052acb3cf53c6e76792cad0287747722
manelaitamer <160795377+ManelAitAmer@users.noreply.github.com>
2024-06-22 11:46:51 +0200

    Add files via upload

---
+++ sakila-activity.sql
@@ -0,0 +1,102 @@
+USE sakila;
+
+-- 2. Retrieve all the data from the tables actor, film and customer.
+
+
+SELECT *
+FROM actor;
+
+SELECT *
+FROM film;
+
+SELECT * 
+from customer;
+
+-- 3. Retrieve the following columns from their respective tables:
+
+-- 3.1 Titles of all films from the film table:
+
+SELECT title
+FROM film;
+
+-- 3.2 List of languages used in films, with the column aliased as language from the language table
+
+SELECT name as language
+FROM language;
+
+-- 3.3 List of first names of all employees from the staff table
+
+ SELECT first_name
+ FROM staff;
+
+-- 4. Retrieve unique release years.
+
+SELECT distinct release_year
+FROM film;
+
+-- Counting records for database insights:
+
+-- 5.1 Determine the number of stores that the company has.
+
+SELECT COUNT(store_id)
+FROM store;
+
+-- 5.2 Determine the number of employees that the company has.
+
+SELECT COUNT(staff_id)
+FROM staff;
+
+-- 5.3 Determine how many films are available for rent and how many have been rented.
+
+SELECT COUNT(*) AS total_films
+FROM film;
+
+SELECT COUNT(*) AS total_copies_available
+FROM inventory;
+
+SELECT COUNT(DISTINCT inventory_id) AS total_rented
+FROM rental;
+
+SELECT 
+    (SELECT COUNT(*) FROM film) AS total_films,
+    (SELECT COUNT(*) FROM inventory) AS total_copies_available,
+    (SELECT COUNT(DISTINCT inventory_id) FROM rental) AS total_rented;
+    
+-- 5.4 Determine the number of distinct last names of the actors in the database.
+
+SELECT COUNT(DISTINCT last_name)
+FROM actor;
+
+-- 6. Retrieve the 10 longest films.
+
+SELECT length
+FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+-- 7. Use filtering techniques in order to:
+
+-- 7.1 Retrieve all actors with the first name "SCARLETT".
+
+SELECT *
+FROM actor
+WHERE first_name =  "SCARLETT";
+
+-- BONUS:
+
+-- 7.2 Retrieve all movies that have ARMAGEDDON in their title and have a duration longer than 100 minutes.
+
+SELECT *
+FROM film
+WHERE title LIKE '%ARMAGEDDON%' AND length > 100;
+
+-- 7.3 Determine the number of films that include Behind the Scenes content
+
+SELECT COUNT(*) AS TOTAL
+from film
+WHERE special_features LIKE '%Behind the Scenes%';
+
+
+
+
+
